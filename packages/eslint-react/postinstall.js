bcea13a74f8c3d091b869be74f5fd479809567b4
sripwoud <me@sripwoud.xyz>
2023-08-14 16:47:33 +0200

    format

--- packages/eslint-react/postinstall.js
+++ packages/eslint-react/postinstall.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const INIT_CWD = process.env.INIT_CWD;
+const INIT_CWD = process.env.INIT_CWD
 
 // See sharing eslint config & npm scoped modules https://eslint.org/docs/latest/developer-guide/shareable-configs#npm-scoped-modules
-const configPath = path.join(INIT_CWD, '.eslintrc.yaml');
+const configPath = path.join(INIT_CWD, '.eslintrc.yaml')
 if (!fs.existsSync(configPath))
-  fs.writeFileSync(configPath, "extends: ['@sripwoud']");
+  fs.writeFileSync(configPath, "extends: ['@sripwoud']")
 
-const destinationPath = path.join(INIT_CWD, '.eslintignore');
+const destinationPath = path.join(INIT_CWD, '.eslintignore')
 if (!fs.existsSync(destinationPath))
-  fs.copyFileSync(path.join(__dirname, '.eslintignore'), destinationPath);
+  fs.copyFileSync(path.join(__dirname, '.eslintignore'), destinationPath)
