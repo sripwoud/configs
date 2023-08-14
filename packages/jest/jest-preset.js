ffd5119e52abd3e995cd4e9ee603fd83507d43dc
sripwoud <me@sripwoud.xyz>
2023-08-14 16:46:19 +0200

    create shared jest config

---
+++ packages/jest/jest-preset.js
@@ -0,0 +1,39 @@
+const tsJestPreset = require('ts-jest/jest-preset')
+
+/**
+ * @type {import('ts-jest').JestConfigWithTsJest}
+ */
+module.exports = {
+  ...tsJestPreset,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!**/*.d.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  moduleDirectories: [
+    'node_modules',
+    '<rootDir>/node_modules',
+    '<rootDir>/*/src',
+    '<rootDir>/src',
+  ],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  setupFilesAfterEnv: [require.resolve('./setup.ts')],
+  testPathIgnorePatterns: [
+    'coverage',
+    'dist',
+    'node_modules',
+    'pnpm-lock.yaml',
+  ],
+  verbose: true,
+  watchPlugins: [
+    'jest-watch-select-projects',
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+}
